69fa9d1e87d047ada884f9fefeb010d534133f3c
Morgan Intrator <morgan.intrator@gmail.com>
2021-05-31 10:31:01 -0400

    feat: rewrite all products for formatting

--- src/tests/products.test.ts
+++ src/tests/products.test.ts
@@ -1,21 +1,16 @@
-import glob from 'glob';
 import fs from 'fs-extra';
-import yaml from 'js-yaml';
+import glob from 'glob';
 import { ProductSchema } from 'src/schemas';
-import { ProductType } from 'src/types';
 
 describe('All products parse', () => {
 
   const filenames = glob.sync('public/api/products/**/*.json')
-    // .slice(0, 1);
 
   filenames.forEach((filename) => {
     it(filename, async () => {
 
       const product = fs.readJSONSync(filename);
       ProductSchema.parse(product)
-
-
       // yamlString = yaml.dump(product, {
       //   'styles': {
       //     '!!null': 'canonical' // dump null as ~
